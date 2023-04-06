c7df7aad3b3b3df2e7025ff78e2a9e00def35fa0
Ahmed Hamzaoui <fikiuhc@gmail.com>
2023-04-06 16:34:36 +0100

    fixed wrong paths

--- .local/bin/cht.sh
+++ .local/bin/cht.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-selected=$(cat ~/.config/tmux/.tmux-cht-languages ~/.config/tmux/.tmux-cht-command | fzf)
+selected=$(cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command | fzf)
 if [[ -z $selected ]]; then
     exit 0
 fi
