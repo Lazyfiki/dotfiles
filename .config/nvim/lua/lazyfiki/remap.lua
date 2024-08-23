e0ae75029687659a14ab30337347e12849b63f18
Ahmed Hamzaoui <lazyfiki@gmail.com>
2024-08-23 23:14:12 +0100

    greatest qol remap ever

--- .config/nvim/lua/lazyfiki/remap.lua
+++ .config/nvim/lua/lazyfiki/remap.lua
@@ -10,6 +10,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set({ "n", "v" }, "p", "p`[=`]")
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
