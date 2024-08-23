280787cd24315a4de78ac569c85896f1d6b34dd5
Ahmed Hamzaoui <lazyfiki@gmail.com>
2024-08-23 23:14:38 +0100

    better quickfix list remaps

--- .config/nvim/lua/lazyfiki/remap.lua
+++ .config/nvim/lua/lazyfiki/remap.lua
@@ -25,9 +25,8 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-o>", "<cmd>silent !tmux neww s<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-
 vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-p>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-y>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
