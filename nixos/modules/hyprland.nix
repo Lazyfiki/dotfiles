f8ff547ac474b61b5b56dcc6f3f286063211986a
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-06-07 11:04:50 +0100

    476 current  2025-06-07 10:40:21  23.11.20240709.205fd42  6.1.96                          *

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -5,10 +5,6 @@
     xwayland.enable = true;
   };
 
-  wayland.windowManager.hyprland.plugins = with pkgs; [
-    unstable.hyprlandPlugins.hy3
-  ];
-
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
