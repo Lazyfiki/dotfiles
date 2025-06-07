e2fe807e66d8f6b91328e62bbb7342672f188529
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-06-07 10:34:58 +0100

    475 current  2025-06-07 10:34:55  23.11.20240709.205fd42  6.1.96                          *

---
+++ nixos/modules/hyprland.nix
@@ -0,0 +1,17 @@
+{
+  programs.hyprland = {
+    enable = true;
+    enableNvidiaPatches = true;
+    xwayland.enable = true;
+  };
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
+  };
+}
