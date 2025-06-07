f32dcfd37f08fd320a7f45dbda55893a2b8fe4df
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-06-07 10:07:56 +0100

    474 current  2025-06-07 09:26:10  23.11.20240709.205fd42  6.1.96                          *

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref={version}";
+    hyprland.url = "github:hyprwm/Hyprland?submodules=1";
     hy3 = {
       url = "github:outfoxxed/hy3?ref=hl{version}";
       inputs.hyprland.follows = "hyprland";
