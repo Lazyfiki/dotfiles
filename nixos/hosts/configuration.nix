15d00489d6fd656d1963400337abb94bb1905846
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-08-24 17:09:35 +0100

    561 current  2025-08-24 13:42:52  23.11.20240709.205fd42  6.1.96                          *

--- nixos/hosts/configuration.nix
+++ nixos/hosts/configuration.nix
@@ -71,6 +71,13 @@
   # i18n.defaultLocale = "en_US.UTF-8";
   i18n.defaultLocale = "ja_JP.UTF-8";
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+    ];
+  };
+
   services.logind = {
     lidSwitch = "suspend";
     lidSwitchDocked = "ignore";
