543ea598b93a0adffcef493d37757389264977f4
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-08-28 20:01:45 +0100

    579 current  2025-08-28 16:47:17  23.11.20240709.205fd42  6.1.96                          *

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "idk";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     quickshell = {
