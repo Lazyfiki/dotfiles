053ac990c4925ab5b06ad80ad59a7972ea784006
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-09-27 09:41:46 +0100

    594 current  2025-09-25 22:28:36  24.11.20250630.50ab793  6.6.94                          *

--- nixos/modules/fcitx5.nix
+++ nixos/modules/fcitx5.nix
@@ -1,10 +1,11 @@
 {pkgs, ...}: {
   i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
+    enable = true;
+    type = "fcitx5";
+    package = pkgs.fcitx5;
+    addons = with pkgs; [
       fcitx5-mozc
       fcitx5-gtk
-      fcitx5-qt
     ];
   };
 }
