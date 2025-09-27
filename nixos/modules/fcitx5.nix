8f419b28bf86274f5b264b6d1a59b984539eefd8
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-09-27 09:58:06 +0100

    596 current  2025-09-27 09:58:03  24.11.20250630.50ab793  6.6.94                          *

--- nixos/modules/fcitx5.nix
+++ nixos/modules/fcitx5.nix
@@ -6,22 +6,9 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
   };
-
-  i18n.inputMethod.ibus = {
-    engines = with pkgs.ibus-engines; [mozc];
-  };
-
-  environment.variables = {
-    GTK_IM_MODULE = "ibus";
-    QT_IM_MODULE = "ibus";
-    XMODIFIERS = "@im=ibus";
-  };
-
-  environment.systemPackages = with pkgs; [
-    fcitx5
-    fcitx5-mozc
-    fcitx5-gtk
-    ibus-engines.mozc
-  ];
 }
