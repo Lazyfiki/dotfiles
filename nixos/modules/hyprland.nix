94412e9d02455e34220c12e928e7f6384f07fb68
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-10-04 15:29:40 +0100

    602 current  2025-10-04 15:29:37  24.11.20250630.50ab793  6.6.94                          *

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -7,7 +7,7 @@
     enable = true;
     xwayland.enable = true;
 
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
