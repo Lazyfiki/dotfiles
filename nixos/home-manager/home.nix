b60eb8eb9c33061c9c597bc967f0102d687231da
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-10-04 16:28:42 +0100

    612 current  2025-10-04 16:28:40  24.11.20250630.50ab793  6.6.94                          *

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -43,6 +43,23 @@
     enable = true;
     userName = "Ahmed Hamzaoui";
     userEmail = "lazyfiki@gmail.com";
+    extraConfig = ''
+      [core]
+          compression = 9
+          whitespace = error
+          preloadindex = true
+
+      [advice]
+          addEmptyPathspec = false
+          pushNonFastForward = false
+          statusHints = false
+
+      [init]
+          defaultBranch = master
+
+      [url "git@github.com:Lazyfiki/"]
+          insteadOf = "ahmed:"
+    '';
   };
 
   # Nicely reload system units when changing configs
