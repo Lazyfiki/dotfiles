80da6f97b58b3ca76f5e38c3453d9d964dd32b5e
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-10-04 15:28:21 +0100

    601 current  2025-10-04 15:28:19  24.11.20250630.50ab793  6.6.94                          *

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -8,7 +8,7 @@
     xwayland.enable = true;
 
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   services.xserver.videoDrivers = ["modesetting"];
