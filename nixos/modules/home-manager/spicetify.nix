b10cf7faad5cf294874a61eedf59fa2562613567
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-10-05 15:39:18 +0100

    640 current  2025-10-05 15:39:14  24.11.20250630.50ab793  6.6.94                          *

--- nixos/modules/home-manager/spicetify.nix
+++ nixos/modules/home-manager/spicetify.nix
@@ -11,7 +11,7 @@ in {
 
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.text;
+    theme = spicePkgs.themes.defaultDynamic;
 
     enabledExtensions = with spicePkgs.extensions; [
       playlistIcons
