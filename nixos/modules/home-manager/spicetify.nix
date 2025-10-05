bcfcf244b5dce2d8cc0c476dbc5bad9816e652f9
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-10-05 15:49:53 +0100

    643 current  2025-10-05 15:43:45  24.11.20250630.50ab793  6.6.94                          *

--- nixos/modules/home-manager/spicetify.nix
+++ nixos/modules/home-manager/spicetify.nix
@@ -11,7 +11,25 @@ in {
 
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.hazy;
+    # theme = spicePkgs.themes.text;
+
+    programs.spicetify.theme = {
+      name = "Bloom";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "nimsandu";
+        repo = "spicetify-bloom";
+        rev = "main";
+        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      };
+
+      injectCss = true;
+      injectThemeJs = true;
+      replaceColors = true;
+      homeConfig = true;
+      overwriteAssets = true;
+      additionalCss = "";
+    };
 
     enabledExtensions = with spicePkgs.extensions; [
       playlistIcons
