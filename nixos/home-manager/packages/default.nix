6e31a53565256c3efc6dcf820e87b7ba2f24c3cd
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-11-06 13:20:34 +0100

    703 current  2025-11-06 13:20:31  24.11.20250630.50ab793  6.6.94                          *

--- nixos/home-manager/packages/default.nix
+++ nixos/home-manager/packages/default.nix
@@ -14,7 +14,7 @@
   home.packages = with pkgs; [
     inputs.quickshell.packages.x86_64-linux.default
     inputs.zen-browser.packages.x86_64-linux.default
-    unstable.input-leap
+    rkvm
     bat
     bc
     bottles
