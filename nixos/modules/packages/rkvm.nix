36f7c69df35df5d5076995cee5c931c6ea1edd76
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-11-07 16:03:58 +0100

    704 current  2025-11-07 16:03:55  24.11.20250630.50ab793  6.6.94                          *

--- nixos/modules/packages/rkvm.nix
+++ nixos/modules/packages/rkvm.nix
@@ -6,8 +6,18 @@
   services.rkvm = {
     enable = true;
     package = pkgs.rkvm;
-    server.enable = true;
+
+    server = {
+      enable = true;
+      settings = {
+        listen = "0.0.0.0:5258";
+        switch-keys = ["left-alt" "left-ctrl"];
+        certificate = "/etc/rkvm/certificate.pem";
+        key = "/etc/rkvm/key.pem";
+        password = "1919est1919";
+      };
+    };
   };
 
-  networking.firewall.allowedTCPPorts = [57892];
+  networking.firewall.allowedTCPPorts = [5258];
 }
