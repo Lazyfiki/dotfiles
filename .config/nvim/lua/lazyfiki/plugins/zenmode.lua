7c37d080f27d878dbf0fe8626be0ff07cbce2db7
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-11-08 13:49:50 +0100

    sum nvim changes

--- .config/nvim/lua/lazyfiki/plugins/zenmode.lua
+++ .config/nvim/lua/lazyfiki/plugins/zenmode.lua
@@ -24,11 +24,22 @@ return {
                 },
             }
             require("zen-mode").toggle()
-            vim.wo.wrap = false
+            vim.wo.wrap = true
+            vim.wo.linebreak = true
+            vim.wo.breakindent = true
+
             vim.wo.number = false
             vim.wo.rnu = false
+
+            vim.keymap.set("n", "j", "gj", { buffer = true })
+            vim.keymap.set("n", "k", "gk", { buffer = true })
             vim.opt.colorcolumn = "0"
+
             ColorMyPencils()
+            local path = vim.fn.expand("~/.cache/wal/colors.json")
+            local colors = vim.fn.json_decode(vim.fn.readfile(path))
+            vim.api.nvim_set_hl(0, "Normal",      { fg = colors.colors.color1, bg = "none" })
+            vim.api.nvim_set_hl(0, "SpellBad", { undercurl = true, sp = "#ff5555", })
         end)
     end
 }
