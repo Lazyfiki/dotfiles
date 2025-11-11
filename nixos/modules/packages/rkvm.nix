d4972c49a6e5027a49e8608259702766592738d7
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-11-11 22:06:22 +0100

    713 current  2025-11-11 22:06:20  24.11.20250630.50ab793  6.6.94                          *

--- nixos/modules/packages/rkvm.nix
+++ nixos/modules/packages/rkvm.nix
@@ -11,7 +11,7 @@
       enable = true;
       settings = {
         listen = "0.0.0.0:5258";
-        switch-keys = ["super" "left-alt"];
+        switch-keys = ["left-alt" "right-alt"];
         certificate = "/etc/rkvm/certificate.pem";
         key = "/etc/rkvm/key.pem";
         password = "1919est1919";
