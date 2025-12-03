f60a796ac753eb404e9818c6f6a1a9a6c1b82ed4
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-12-03 16:39:35 +0100

    714 current  2025-11-11 22:14:59  24.11.20250630.50ab793  6.6.94                          *

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
